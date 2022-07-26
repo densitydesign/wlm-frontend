a9eee08e28fc9c196b1c2c4aae6e5097404acbc9
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-07-26 21:44:40 +0200

    init MapVentagli component

---
+++ components/MapVentagli/MapVentagli.js
@@ -0,0 +1,4 @@
+import styles from "MapVentagli.module.scss";
+export default function MapVentagli() {
+	return <p>MapVentagli</p>;
+}
---
+++ components/MapVentagli/index.js
@@ -0,0 +1,2 @@
+import MapVentagli from "./MapVentagli";
+export default MapVentagli;
