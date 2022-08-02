8c6b73832e692577f374086de500c608d1e9c7e1
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-08-02 19:08:09 +0200

    Update VisualizationController.js

--- components/VisualizationController/VisualizationController.js
+++ components/VisualizationController/VisualizationController.js
@@ -7,10 +7,15 @@ import { Col, Container, Row } from "react-bootstrap";
 import classNames from "classnames";
 import MapVentagli from "../MapVentagli/MapVentagli";
 import { feature } from "topojson-client";
+import { ButtonUI, DatePickerUI, DropdownUI } from "../UI-Components";
+import { BsDownload } from "react-icons/bs";
 
 export default function VisualizationController() {
 	const { asPath, basePath } = useRouter();
 	const [data, setData] = useState();
+	const [regionsList, setRegionsList] = useState();
+	const [provincesList, setProvincesList] = useState();
+	const [municipalitiesList, setMunicipalitiesList] = useState();
 
 	const [play, setPlay] = useState(false);
 	const [slice, setSlice] = useState();
@@ -20,21 +25,49 @@ export default function VisualizationController() {
 
 	useEffect(() => {
 		// fetch initial geography
-		const urlGeo = `${basePath}/api-data-simulated/geometries/Reg01012022_WGS84_topoJSON.json`;
+		const geoReg = d3.json(`${basePath}/api-data-simulated/geometries/Reg01012022_WGS84_topoJSON.json`);
+		const geoProv = d3.json(`${basePath}/api-data-simulated/geometries/ProvCM01012022_WGS84_topoJSON.json`);
+		const geoCom = d3.json(`${basePath}/api-data-simulated/geometries/Com01012022_WGS84_topoJSON.json`);
 
 		// fetch initial data
-		const urlData = `${basePath}/api-data-simulated/interval-12months.aggregation-region.json`;
+		const _data = d3.json(`${basePath}/api-data-simulated/interval-12months.aggregation-region.json`);
 
-		Promise.all([d3.json(urlGeo), d3.json(urlData)]).then((values) => {
-			const topoJson = values[0];
-			const object = Object.keys(topoJson.objects)[0];
-			const geographies = feature(topoJson, topoJson.objects[object]);
-			const ventagli = values[1];
-			const codeKey = "COD_REG"
-			const labelKey = "DEN_REG"
-			const max = d3.max(ventagli, d=>d[1][d[1].length-1][1][0].valueIncremental)
-			const extent = [0, max]
-			setData({ ventagli, geographies, codeKey, labelKey, extent });
+		Promise.all([geoReg, geoProv, geoCom, _data]).then(([regionsTopo, provincesTopo, municipalitiesTopo, ventagli]) => {
+			let _key = Object.keys(regionsTopo.objects)[0];
+			const geographiesRegions = feature(regionsTopo, regionsTopo.objects[_key]);
+			const _regionsList = geographiesRegions.features.map((d) => ({
+				label: d.properties.DEN_REG,
+				code: d.properties.COD_REG,
+			}));
+			setRegionsList(_regionsList)
+
+			_key = Object.keys(provincesTopo.objects)[0];
+			const geographiesProvinces = feature(provincesTopo, provincesTopo.objects[_key]);
+			const _provincesList = geographiesProvinces.features.map((d) => ({
+				label: d.properties.DEN_UTS,
+				code: d.properties.COD_UTS,
+				codeRegion: d.properties.COD_REG,
+			}));
+			setProvincesList(_provincesList)
+
+			_key = Object.keys(municipalitiesTopo.objects)[0];
+			const geographiesMunicipalities = feature(municipalitiesTopo, municipalitiesTopo.objects[_key]);
+			const _municipalitiesList = geographiesMunicipalities.features.map((d) => ({
+				label: d.properties.COMUNE,
+				code: d.properties.PRO_COM,
+				codeProvince: d.properties.COD_UTS,
+				codeRegion: d.properties.COD_REG,
+			}));
+			setMunicipalitiesList(_municipalitiesList)
+
+			const codeKey = "COD_REG";
+			const labelKey = "DEN_REG";
+
+			console.log(geographiesMunicipalities)
+
+			const max = d3.max(ventagli, (d) => d[1][d[1].length - 1][1][0].valueIncremental);
+			const extent = [0, max];
+			setData({ ventagli, geographies: geographiesRegions, codeKey, labelKey, extent });
 		});
 	}, []);
 
@@ -66,13 +99,22 @@ export default function VisualizationController() {
 		<Container className={classNames(styles.vizController)} fluid>
 			<Row className={classNames("h-100")}>
 				<Col className={classNames("h-100")} md={3} xl={3}>
-					sidebar
+					<DropdownUI label="Theme" defaultLabel="All monuments" />
+					<DropdownUI items={regionsList} defaultLabel="Select a region" />
+					<DropdownUI items={provincesList} defaultLabel="Select a province" />
+					<DropdownUI items={municipalitiesList} defaultLabel="Select a municipality"/>
+					<DatePickerUI label="From" />
+					<DatePickerUI label="To" />
+					<strong>ButtonGroup</strong>
+					<ButtonUI label="Save" content={<BsDownload />} />
+					
+					{/* sidebar
 					<input
 						type="checkbox"
 						checked={play}
 						onChange={handleOnChangePlay}
 					/>{" "}
-					Play
+					Play */}
 				</Col>
 				<Col className={classNames("h-100")}>
 					<MapVentagli data={data} />
