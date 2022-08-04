f75758d88cd1ce9a057ec0173079a9f6f895a00b
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-08-04 15:11:55 +0200

    Update NavMenu.js

--- components/NavMenu/NavMenu.js
+++ components/NavMenu/NavMenu.js
@@ -1,5 +1,16 @@
-import styles from "./NavMenu.module.scss"
+import Link from "next/link";
+import styles from "./NavMenu.module.scss";
+import { HiMenu } from "react-icons/hi";
+import classNames from "classnames";
 
-export default function NavMenu(){
-  return <div><h5>W{"<"}3M</h5></div>
+export default function NavMenu() {
+	return (
+		<div className={classNames("d-flex", "justify-content-start", "align-items-center", "mb-3")}>
+			<h5>
+				<Link href="/">W</Link>
+				{"<"}3<Link href="/page">M</Link>
+			</h5>
+			<HiMenu />
+		</div>
+	);
 }
