2f2f186c503fb09d997cdd905662ae695997f1c7
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-08-26 17:33:20 +0200

    Added noto sans font

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,20 @@ import "../styles/globals.scss";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
-import SSRProvider from 'react-bootstrap/SSRProvider'
+import SSRProvider from "react-bootstrap/SSRProvider";
+
+import "@fontsource/noto-sans";
 
 function MyApp({ Component, pageProps }) {
 	const router = useRouter();
-  useEffect(() => {
-    typeof document !== undefined
-      ? require("bootstrap/dist/js/bootstrap")
-      : null;
-  }, [router.events]);
-  return <SSRProvider><Component {...pageProps} /></SSRProvider>;
+	useEffect(() => {
+		typeof document !== undefined ? require("bootstrap/dist/js/bootstrap") : null;
+	}, [router.events]);
+	return (
+		<SSRProvider>
+			<Component {...pageProps} />
+		</SSRProvider>
+	);
 }
 
 export default MyApp;
