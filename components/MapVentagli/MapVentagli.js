c00188513584692aa7d32140cd825991838b4898
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-09-20 10:50:49 +0200

    Fix warning

--- components/MapVentagli/MapVentagli.js
+++ components/MapVentagli/MapVentagli.js
@@ -68,7 +68,8 @@ export default function MapVentagli({
 
 	const svg_ns = {
 		"xmlns": "http://www.w3.org/2000/svg",
-		"xmlns:xlink": "http://www.w3.org/1999/xlink"
+		"version": "1.1"
+		// "xmlns:xlink": "http://www.w3.org/1999/xlink"
 	};
 
 	return (
