96614c752a2d9e3302a5b6de784b853d3ad80ea5
bndndr <benedetti.a93@gmail.com>
2022-09-22 16:59:19 +0200

    metatags

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,21 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Home</title>
+				<title>WikiLovesMonuments - Data visualization</title>
+				<meta name="title" content="WikiLovesMonuments - Data visualization" />
+				<meta name="description" content="Explore the data about photographed monuments from WikiLovesMonuments, in the data visualization system developed using WikiData." />
+
+				<meta property="og:type" content="website" />
+				<meta property="og:url" content="https://data.wikilovesmonuments.it/" />
+				<meta property="og:title" content="WikiLovesMonuments - Data visualization" />
+				<meta property="og:description" content="Explore the data about photographed monuments from WikiLovesMonuments, in the data visualization system developed using WikiData. " />
+				<meta property="og:image" content="images/og-image.png" />
+
+				<meta property="twitter:card" content="summary_large_image" />
+				<meta property="twitter:url" content="https://data.wikilovesmonuments.it/" />
+				<meta property="twitter:title" content="WikiLovesMonuments - Data visualization" />
+				<meta property="twitter:description" content="Explore the data about photographed monuments from WikiLovesMonuments, in the data visualization system developed using WikiData. " />
+				<meta property="twitter:image" content="images/og-image.png" />
 			</Head>
       {/* <Navigation/> */}
 			<VisualizationController/>
