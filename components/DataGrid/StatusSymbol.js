eac240f69159c747738cab1631979c164dbb0988
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-10-14 15:53:56 +0200

    update react bootstrap

--- components/DataGrid/StatusSymbol.js
+++ components/DataGrid/StatusSymbol.js
@@ -7,38 +7,18 @@ export default function StatusSymbol({ status }) {
   const target = useRef(null);
   return (
     <>
-      {/* <span
-        ref={target}
-        onClick={() => setShow(!show)}
-        style={{
-          display: "block",
-          width: "0.7rem",
-          height: "0.7rem",
-          backgroundColor: colors[status],
-          borderRadius: "50%",
-          border: "1px solid #333",
-        }}
-      >sss</span>
-      <Overlay target={target.current} show={show} placement="right" >
-        {(props) => (
-          <Tooltip id="overlay-example" {...props}>
-            {status}
-          </Tooltip>
-        )}
-      </Overlay> */}
-    
-    <OverlayTrigger placement={"right"} overlay={<Tooltip>{status}</Tooltip>}>
-      <span
-        style={{
-          display: "inline-block",
-          width: "1rem",
-          height: "1rem",
-          backgroundColor: colors[status],
-          borderRadius: "50%",
-          border: "1px solid #333",
-        }}
-      />
-    </OverlayTrigger>
+      <OverlayTrigger placement={"right"} overlay={<Tooltip>{status}</Tooltip>}>
+        <span
+          style={{
+            display: "inline-block",
+            width: "1rem",
+            height: "1rem",
+            backgroundColor: colors[status],
+            borderRadius: "50%",
+            border: "1px solid #333",
+          }}
+        />
+      </OverlayTrigger>
     </>
   );
 }
