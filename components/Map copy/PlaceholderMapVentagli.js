c11fb8e27dcd871b2fa09bd914946149efb9ff32
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-12-12 10:37:39 +0100

    removed unused component

--- components/Map copy/PlaceholderMapVentagli.js
+++
@@ -1,6 +0,0 @@
-import styles from "./MapVentagli.module.scss";
-import classNames from "classnames";
-
-export default function PlaceholderMapVentagli() {
-	return <div className={classNames(styles.map, styles.placeholder, "position-relative")} />;
-}
--- components/Map copy/index.js
+++
@@ -1,4 +0,0 @@
-import MapVentagli from "./MapVentagli";
-import PlaceholderMapVentagli from "./PlaceholderMapVentagli";
-export default MapVentagli;
-export { MapVentagli, PlaceholderMapVentagli };
