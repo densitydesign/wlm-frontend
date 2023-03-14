179aba774529e0db6ba85d9893eab3af323cdecb
iosonosempreio <iosonosempreio@users.noreply.github.com>
2023-03-14 22:44:54 +0100

    machine readable license tag

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,12 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <footer
+          style={{ display: "none" }}
+          dangerouslySetInnerHTML={{
+            __html: `<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work by <a xmlns:cc="http://creativecommons.org/ns#" href="https://data.wikilovesmonuments.it/about/" property="cc:attributionName" rel="cc:attributionURL">DensityDesign (Politecnico di Milano) & Wikimedia Italia</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>`,
+          }}
+        ></footer>
       </body>
     </Html>
   );
